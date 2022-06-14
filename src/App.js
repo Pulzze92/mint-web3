689a54e83a241fe2839f9999c9a1ab1f8f9495ca
Pulzze92 <89998310392@mail.ru>
2022-06-14 14:18:47 +0300

    feat: add styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import React from 'react';
 import './App.css';
+import MainMint from './MainMint';
+import NavBar from './NavBar';
 
 function App() {
-  return ( <
-    div className = "App" >
+  const [accounts, setAccounts] = React.useState([]);
 
-    <
-    /div>
+  return (
+    <div className="overlay">
+      <div className="App">
+        <NavBar accounts={accounts} setAccounts={setAccounts} />{' '}
+        <MainMint accounts={accounts} setAccounts={setAccounts} />{' '}
+      </div>{' '}
+      <div className="moving-background"> </div>{' '}
+    </div>
   );
 }
 
